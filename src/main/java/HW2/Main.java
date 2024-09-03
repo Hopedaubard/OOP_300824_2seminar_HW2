81781c76cb8913e35795261ee03f2d21b21c0961
Alex <trainofbird@gmail.com>
2024-09-03 23:01:32 +0300

    HW2 completed

--- src/main/java/HW2/Main.java
+++ src/main/java/HW2/Main.java
@@ -1,7 +1,18 @@
 package HW2;
 
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
         Human human1 = new Human("Saul");
+        Human human2 = new Human("Max");
+        Human human3 = new Human("Peter");
+        Human human4 = new Human("Diter");
+        Market market = new Market();
+        market.takeInQueue(human1);
+        market.takeInQueue(human2);
+        market.takeInQueue(human3);
+        market.takeInQueue(human4);
+        market.update();
     }
 }
--- src/main/java/HW2/Market.java
+++ src/main/java/HW2/Market.java
@@ -1,50 +1,84 @@
 package HW2;
 
+import java.util.ArrayList;
 import java.util.List;
 
-public class Market implements QueueBehaviour, MarketBehaviour{
+public class Market implements QueueBehaviour, MarketBehaviour {
     private List<Actor> queue;
+    private List<Actor> releaseQueue;
+
+    public Market() {
+        this.queue = new ArrayList<>();
+    }
     @Override
     public void acceptToMarket(Actor actor) {
-
-//актор пришел в магазин
-        // добавить актора в очередь
-        //вызов метода для добавления в очередь
+        System.out.println(actor.getName() + " пришел в магазин");
+        //добавить актора в очередь вызовом метода для добавления в очередь
+        takeInQueue(actor);
     }
 
     @Override
     public void releaseFromMarket(List<Actor> actorList) {
-//удаляем посетителей из списка в переменной (из магазина)
+        for (Actor actor:actorList) {
+            queue.remove(actor);
+            System.out.println(actor.getName() + " покинул магазин.");
+        }
+        //удаляем посетителей из списка в переменной (из магазина)
     }
 
     @Override
     public void update() {
-//тейк ордерс
+        //тейк ордерс
+        takeOrders();
         //гив ордерс
-        // выйти из очереди
+        giveOrders();
+        //выйти из очереди
+        releaseFromQueue();
     }
 
     @Override
     public void takeInQueue(Actor actor) {
-//добавить посетителя в очередь
+        System.out.println(actor.getName() + " встал в очередь");
+        //добавить посетителя в очередь
+        this.queue.add(actor);
     }
 
     @Override
     public void takeOrders() {
-//пройти по всему списку очереди
+        for (Actor actor:queue) {
+            if (!actor.isTakeOrder){
+                actor.setTakeOrder(true);
+                System.out.println("Заказ " + actor.getName() + " принят!");
+            }
+        }
+        //пройти по всему списку очереди
         //проверять методом измайкодрер тех, кто заказ не сделал
-        //проставлять флаг с помолью метода гет ордерс
+        //проставлять флаг с помощью метода сет ордерс
     }
 
     @Override
     public void giveOrders() {
-//пробегаемся по всему списку нашших заказчиков
+        for (Actor actor:queue) {
+            if (!actor.isMakeOrder()){
+                actor.setTakeOrder(true);
+                System.out.println("Заказ " + actor.getName() + " исполнен! Клиент получил свой заказ.");
+            }
+        }
+        //пробегаемся по всему списку наших заказчиков
         //проверяем тех у кого флак измейкордерс ТРУ
         // Ставим изтейкордер ТРУ
     }
 
     @Override
     public void releaseFromQueue() {
-//составить список акторов, которые готовы выходить из магазина
+        List<Actor> releaseQueue = new ArrayList<>();
+        for (Actor actor:queue) {
+            if (actor.isTakeOrder()){
+                releaseQueue.add(actor);
+                System.out.println(actor.getName() + " покинул очередь.");
+            }
+        }
+        releaseFromMarket(releaseQueue);
+        //составить список акторов, которые готовы выходить из магазина
     }
 }
